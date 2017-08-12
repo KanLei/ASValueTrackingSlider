e847b7b4ede6778d7f5f39cd6e103a6a461fbf77
leikan <kanlei@live.com>
2017-08-13 02:13:03 +0800

    init

---
+++ ASValueTrackingSlider/ValueTrackingSlider/ValuePopUpView.cs
@@ -0,0 +1,403 @@
+﻿
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// This UIView subclass is used internally by ASValueTrackingSlider
+// The public API is declared in ASValueTrackingSlider.h
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+using System;
+using UIKit;
+using Foundation;
+using CoreGraphics;
+using CoreAnimation;
+using ObjCRuntime;
+using System.Collections.Generic;
+
+
+namespace ASValueTrackingSlider.ValueTrackingSlider
+{
+	public static class Extensions
+	{
+		public static void AnimateKey(this CALayer layer, NSString animationName, NSObject fromValue, NSObject toValue, Action<CABasicAnimation> block)
+		{
+			layer.SetValueForKey(toValue, animationName);
+			var anim = CABasicAnimation.FromKeyPath(animationName);
+			anim.From = fromValue ?? layer.PresentationLayer.ValueForKey(animationName);
+			anim.To = toValue;
+			block?.Invoke(anim);
+			layer.AddAnimation(anim, animationName);
+		}
+	}
+
+
+	public interface IASValuePopUpViewDelegate
+	{
+		nfloat CurrentValueOffset { get; }  // expects value in the range 0.0 - 1.0
+		void ColorDidUpdate(UIColor opaqueColor);
+	}
+
+	public class ValuePopUpView : UIView
+	{
+		public const string SliderFillColorAnim = "fillColor";
+
+		public IASValuePopUpViewDelegate Delegate { get; set; }
+
+		private bool shouldAnimate;
+		private double animDuration;
+		private NSMutableAttributedString attributedString;
+		private CAShapeLayer pathLayer;
+		private CATextLayer textLayer;
+		private nfloat arrowCenterOffset;
+
+		// never actually visible, its purpose is to interpolate color values for the popUpView color animation
+		// using shape layer because it has a 'fillColor' property which is consistent with _backgroundLayer
+		private CAShapeLayer colorAnimLayer;
+
+		public ValuePopUpView(CGRect frame) : base(frame)
+		{
+			shouldAnimate = false;
+			Layer.AnchorPoint = new CGPoint(0.5, 1);
+
+			UserInteractionEnabled = false;
+			pathLayer = (CAShapeLayer)this.Layer;
+
+			cornerRadius = 4.0f;
+			ArrowLength = 13.0f;
+			WidthPaddingFactor = 1.15f;
+			HeightPaddingFactor = 1.1f;
+
+			textLayer = new CATextLayer();
+			textLayer.AlignmentMode = CATextLayer.AlignmentCenter;
+			textLayer.AnchorPoint = new CGPoint(0, 0);
+			textLayer.ContentsScale = UIScreen.MainScreen.Scale;
+			textLayer.Actions = NSDictionary.FromObjectAndKey(NSNull.Null, new NSString("contents"));
+
+			colorAnimLayer = new CAShapeLayer();
+
+			Layer.AddSublayer(colorAnimLayer);
+			Layer.AddSublayer(textLayer);
+
+			attributedString = new NSMutableAttributedString(" ", new NSDictionary());
+		}
+
+		public ValuePopUpView(IntPtr handle) : base(handle)
+		{
+
+		}
+
+		private nfloat cornerRadius;
+		public nfloat CornerRadius
+		{
+			get { return cornerRadius; }
+			set
+			{
+				if (cornerRadius == value) return;
+				cornerRadius = value;
+				pathLayer.Path = PathForRect(this.Bounds, arrowCenterOffset)?.CGPath;
+			}
+		}
+
+		public nfloat ArrowLength { get; set; }
+		public nfloat WidthPaddingFactor { get; set; }
+		public nfloat HeightPaddingFactor { get; set; }
+
+
+		public UIColor Color
+		{
+			get
+			{
+				var sl = pathLayer.PresentationLayer as CAShapeLayer;
+				return UIColor.FromCGColor(sl.FillColor);
+			}
+			set
+			{
+				pathLayer.FillColor = value.CGColor;
+				colorAnimLayer.RemoveAnimation(SliderFillColorAnim); // single color, no animation required
+			}
+		}
+
+
+		public UIColor OpaqueColor
+		{
+			get
+			{
+				var sl = colorAnimLayer.PresentationLayer as CAShapeLayer;
+				var fc = sl == null ? pathLayer.FillColor : sl.FillColor;
+				return OpaqueUIColorFromCGColor(fc);
+			}
+		}
+
+
+		public void SetTextColor(UIColor color)
+		{
+			textLayer.ForegroundColor = color.CGColor;
+		}
+
+		public void SetFont(UIFont font)
+		{
+			attributedString.AddAttribute(UIStringAttributeKey.Font, font, new NSRange(0, attributedString.Length));
+			textLayer.SetFont(font.Name);
+			textLayer.FontSize = font.PointSize;
+		}
+
+		public void SetText(string text)
+		{
+			attributedString.MutableString.SetString(new NSString(text));
+			textLayer.String = text;
+		}
+
+		// set up an animation, but prevent it from running automatically
+		// the animation progress will be adjusted manually
+		public void SetAnimatedColors(UIColor[] animatedColors, NSNumber[] keyTimes)
+		{
+			var cgColors = new List<NSObject>();
+			foreach (var col in animatedColors)
+			{
+				cgColors.Add(NSObject.FromObject(col.CGColor));
+			}
+
+			var colorAnim = CAKeyFrameAnimation.FromKeyPath(SliderFillColorAnim);
+			colorAnim.KeyTimes = keyTimes;
+			colorAnim.Values = cgColors.ToArray();
+			colorAnim.FillMode = CAFillMode.Both;
+			colorAnim.Duration = 1.0;
+			colorAnim.Delegate = new AnimationDelegate(this);
+
+			// As the interpolated color values from the presentationLayer are needed immediately
+			// the animation must be allowed to start to initialize _colorAnimLayer's presentationLayer
+			// hence the speed is set to min value - then set to zero in 'animationDidStart:' delegate method
+			colorAnimLayer.Speed = 1.175494351e-38F;  // FLT_MIN
+													  //colorAnimLayer.Speed = 0.0000000000000000000000000000000000000117549435f;
+			colorAnimLayer.TimeOffset = 0.0;
+
+			colorAnimLayer.AddAnimation(colorAnim, SliderFillColorAnim);
+		}
+
+		public void SetAnimationOffset(nfloat animOffset, Action<UIColor> block)
+		{
+			if (colorAnimLayer.AnimationForKey(SliderFillColorAnim) != null)
+			{
+				colorAnimLayer.TimeOffset = animOffset;
+				pathLayer.FillColor = ((CAShapeLayer)colorAnimLayer.PresentationLayer).FillColor;
+				block?.Invoke(OpaqueColor);
+			}
+		}
+
+		public void SetFrame(CGRect frame, nfloat arrowOffset, string text)
+		{
+			// only redraw path if either the arrowOffset or popUpView size has changed
+			if (arrowOffset != arrowCenterOffset || !(frame.Size == this.Frame.Size))
+			{
+				pathLayer.Path = PathForRect(frame, arrowOffset).CGPath;
+			}
+			arrowCenterOffset = arrowOffset;
+
+			nfloat anchorX = 0.5f + (arrowOffset / frame.Width);
+			Layer.AnchorPoint = new CGPoint(anchorX, 1);
+			Layer.Position = new CGPoint(frame.GetMinX() + frame.Width * anchorX, 0);
+			Layer.Bounds = new CGRect(CGPoint.Empty, frame.Size);
+
+			SetText(text);
+		}
+
+		// _shouldAnimate = YES; causes 'actionForLayer:' to return an animation for layer property changes
+		// call the supplied block, then set _shouldAnimate back to NO
+		public void AnimateBlock(Action<double> block)
+		{
+			shouldAnimate = true;
+			animDuration = 0.5f;
+
+			var anim = Layer.AnimationForKey("position");
+			if (anim != null)
+			{
+				double elapsedTime = Math.Min(CAAnimation.CurrentMediaTime() - anim.BeginTime, anim.Duration);
+				animDuration = animDuration * elapsedTime / anim.Duration;
+			}
+
+			block?.Invoke(animDuration);
+			shouldAnimate = false;
+		}
+
+		public CGSize PopUpSizeForString(string str)
+		{
+			attributedString.MutableString.SetString(new NSString(str));
+			nfloat w, h;
+			w = NMath.Ceiling(attributedString.Size.Width * WidthPaddingFactor);
+			h = NMath.Ceiling(attributedString.Size.Height * HeightPaddingFactor + ArrowLength);
+			return new CGSize(w, h);
+		}
+
+		public void ShowAnimated(bool animated)
+		{
+			if (!animated)
+			{
+				Layer.Opacity = 1.0f;
+				return;
+			}
+
+			CATransaction.Begin();
+			{
+				// start the transform animation from scale 0.5, or its current value if it's already running
+				NSObject fromValue = Layer.AnimationForKey("transform") != null
+										 ? Layer.PresentationLayer.ValueForKey(new NSString("transform"))
+										 : NSValue.FromCATransform3D(CATransform3D.MakeScale(0.5f, 0.5f, 1));
+
+				Layer.AnimateKey(new NSString("transform"),
+											 fromValue,
+											 NSValue.FromCATransform3D(CATransform3D.Identity),
+											 (animation) =>
+											 {
+												 animation.Duration = 0.4;
+												 animation.TimingFunction = CAMediaTimingFunction.FromControlPoints(0.8f, 2.5f, 0.35f, 0.5f);
+											 });
+				Layer.AnimateKey(new NSString("opacity"), null, NSObject.FromObject(1.0), (animation) =>
+				 {
+					 animation.Duration = 0.1;
+				 });
+			}
+			CATransaction.Commit();
+		}
+
+		public void HideAnimated(bool animated, Action completion)
+		{
+			CATransaction.Begin();
+			{
+				CATransaction.CompletionBlock = () =>
+				{
+					completion?.Invoke();
+					Layer.Transform = CATransform3D.Identity;
+				};
+
+				if (animated)
+				{
+					Layer.AnimateKey(new NSString("transform"), null,
+									 NSValue.FromCATransform3D(CATransform3D.MakeScale(0.5f, 0.5f, 1)),
+									 (animation) =>
+									 {
+										 animation.Duration = 0.55;
+										 animation.TimingFunction = CAMediaTimingFunction.FromControlPoints(0.1f, -2, 0.3f, 3);
+									 });
+					Layer.AnimateKey(new NSString("opacity"), null,
+									 NSObject.FromObject(0.0), (animation) =>
+									 {
+										 animation.Duration = 0.75;
+									 });
+				}
+				else // not animated - just set opacity to 0.0
+				{
+					Layer.Opacity = 0.0f;
+				}
+			}
+			CATransaction.Commit();
+		}
+
+		[Export("layerClass")]
+		public static Class LayerClass()
+		{
+			return new Class(typeof(CAShapeLayer));
+		}
+
+		// if ivar _shouldAnimate) is YES then return an animation
+		// otherwise return NSNull (no animation)
+		public override NSObject ActionForLayer(CALayer layer, string eventKey)
+		{
+			if (shouldAnimate)
+			{
+				var anim = CABasicAnimation.FromKeyPath(eventKey);
+				anim.BeginTime = CAAnimation.CurrentMediaTime();
+				anim.TimingFunction = CAMediaTimingFunction.FromName(CAMediaTimingFunction.EaseInEaseOut);
+				anim.From = layer.PresentationLayer.ValueForKey(new NSString(eventKey));
+				anim.Duration = animDuration;
+				return anim;
+			}
+			else
+			{
+				return NSNull.Null;
+			}
+		}
+
+		private UIBezierPath PathForRect(CGRect rect, nfloat arrowOffset)
+		{
+			if (rect == CGRect.Empty) return null;
+			rect = new CGRect(CGPoint.Empty, rect.Size);  // ensure origin is CGPointZero
+
+			// Create rounded rect
+			CGRect roundedRect = rect;
+			var size = roundedRect.Size;
+			size.Height -= ArrowLength;
+			roundedRect.Size = size;
+			var popUpPath = UIBezierPath.FromRoundedRect(roundedRect, cornerRadius);
+
+			// Create arrow path
+			nfloat maxX = roundedRect.GetMaxX();  // prevent arrow from extending beyond this point
+			nfloat arrowTipX = rect.GetMidX() + arrowOffset;
+			CGPoint tip = new CGPoint(arrowTipX, rect.GetMaxY());
+
+			nfloat arrowLength = roundedRect.Height / 2.0f;
+			nfloat x = arrowLength * NMath.Tan(45.0f * NMath.PI / 180);  // x = half the length of the base of the arrow
+
+			var arrowPath = new UIBezierPath();
+			arrowPath.MoveTo(tip);
+			arrowPath.AddLineTo(new CGPoint(NMath.Max(arrowTipX - x, 0), roundedRect.GetMaxY() - arrowLength));
+			arrowPath.AddLineTo(new CGPoint(NMath.Min(arrowTipX + x, maxX), roundedRect.GetMaxY() - arrowLength));
+			arrowPath.ClosePath();
+
+			popUpPath.AppendPath(arrowPath);
+
+			return popUpPath;
+		}
+
+		public override void LayoutSubviews()
+		{
+			base.LayoutSubviews();
+
+			nfloat textHeight = attributedString.Size.Height;
+			CGRect textRect = new CGRect(this.Bounds.X,
+										 (this.Bounds.Size.Height - ArrowLength - textHeight) / 2.0f,
+										  this.Bounds.Size.Width,
+										  textHeight);
+			textLayer.Frame = textRect.Integral();
+		}
+
+		private static UIColor OpaqueUIColorFromCGColor(CGColor col)
+		{
+			if (col == null) return null;
+
+			nfloat[] components = col.Components;
+			UIColor color;
+			if (col.Components.Length == 2)
+			{
+				color = UIColor.FromWhiteAlpha(components[0], 1);
+			}
+			else
+			{
+				color = UIColor.FromRGBA(components[0], components[1], components[2], 1);
+			}
+			return color;
+		}
+
+
+
+		private class AnimationDelegate : CAAnimationDelegate
+		{
+			// TODO: weakreference
+			private readonly ValuePopUpView popUpView;
+			public AnimationDelegate(ValuePopUpView popUpView)
+			{
+				this.popUpView = popUpView;
+			}
+
+			// set the speed to zero to freeze the animation and set the offset to the correct value
+			// the animation can now be updated manually by explicity setting its 'timeOffset'
+			public override void AnimationStarted(CAAnimation anim)
+			{
+				popUpView.colorAnimLayer.Speed = 0.0f;
+				popUpView.colorAnimLayer.TimeOffset = popUpView.Delegate.CurrentValueOffset;
+
+				popUpView.pathLayer.FillColor = ((CAShapeLayer)popUpView.colorAnimLayer.PresentationLayer).FillColor;
+				popUpView.Delegate.ColorDidUpdate(popUpView.OpaqueColor);
+			}
+		}
+	}
+}
+
---
+++ ASValueTrackingSlider/ValueTrackingSlider/ValueTrackingSlider.cs
@@ -0,0 +1,507 @@
+﻿
+using System;
+using System.Linq;
+using UIKit;
+using Foundation;
+using CoreGraphics;
+using System.Collections.Generic;
+
+namespace ASValueTrackingSlider.ValueTrackingSlider
+{
+	public interface IASValueTrackingSliderDataSource
+	{
+		string Slider(ValueTrackingSlider slider, float value);
+	}
+
+	public interface IASValueTrackingSliderDelegate
+	{
+		void SliderWillDisplayPopUpView(ValueTrackingSlider slider);
+		void SliderWillHidePopUpView(ValueTrackingSlider slider);
+		void SliderDidHidePopUpView(ValueTrackingSlider slider);
+	}
+
+	[Register(nameof(ValueTrackingSlider))]
+	public class ValueTrackingSlider : UISlider, IASValuePopUpViewDelegate
+	{
+		public IASValueTrackingSliderDataSource DataSource { get; set; }
+		public IASValueTrackingSliderDelegate Delegate { get; set; }
+
+		private ValuePopUpView PopUpView { get; set; }
+		private bool PopUpViewAlwaysOn { get; set; }
+
+		private NSNumber[] keyTimes;
+		private nfloat valueRange;
+
+		public ValueTrackingSlider(CGRect frame) : base(frame)
+		{
+			Setup();
+		}
+
+		[Export("initWithCoder:")]
+		public ValueTrackingSlider(NSCoder coder) : base(coder)
+		{
+			Setup();
+		}
+
+		public ValueTrackingSlider(IntPtr handle) : base(handle)
+		{
+
+		}
+
+		private bool autoAdjustTrackColor;
+		public bool AutoAdjustTrackColor
+		{
+			get
+			{
+				return autoAdjustTrackColor;
+			}
+			set
+			{
+				if (AutoAdjustTrackColor == value) return;
+
+				AutoAdjustTrackColor = value;
+
+				// setMinimumTrackTintColor has been overridden to also set autoAdjustTrackColor to NO
+				// therefore super's implementation must be called to set minimumTrackTintColor
+				if (!value)
+				{
+					base.MinimumTrackTintColor = null;  // sets track to default blue color
+				}
+				else
+				{
+					base.MinimumTrackTintColor = PopUpView.OpaqueColor;
+				}
+			}
+		}
+
+		private UIColor textColor;
+		public UIColor TextColor
+		{
+			get { return textColor; }
+			set
+			{
+				textColor = value;
+				PopUpView.SetTextColor(value);
+			}
+		}
+
+		private UIFont font;
+		public UIFont Font
+		{
+			get { return font; }
+			set
+			{
+				font = value;
+				PopUpView.SetFont(font);
+			}
+		}
+
+		// setting the value of 'popUpViewColor' overrides 'popUpViewAnimatedColors' and vice versa
+		// the return value of 'popUpViewColor' is the currently displayed value
+		// this will vary if 'popUpViewAnimatedColors' is set (see below)
+		private UIColor popUpViewColor;
+		public UIColor PopUpViewColor
+		{
+			get
+			{
+				return PopUpView.Color ?? popUpViewColor;
+			}
+			set
+			{
+				popUpViewColor = value;
+				PopUpViewAnimatedColors = null; // animated colors should be discarded
+				PopUpView.Color = value;
+
+				if (AutoAdjustTrackColor)
+				{
+					base.MinimumTrackTintColor = PopUpView.OpaqueColor;
+				}
+			}
+		}
+
+		// pass an array of 2 or more UIColors to animate the color change as the slider moves
+		private UIColor[] popUpViewAnimatedColors;
+		public UIColor[] PopUpViewAnimatedColors
+		{
+			get { return popUpViewAnimatedColors; }
+			set
+			{
+				popUpViewAnimatedColors = value;
+				SetPopUpViewAnimatedColors(value, null);
+			}
+		}
+
+
+		// if 2 or more colors are present, set animated colors
+		// if only 1 color is present then call 'setPopUpViewColor:'
+		// if arg is nil then restore previous _popUpViewColor
+		public void SetPopUpViewAnimatedColors(UIColor[] colors, NSNumber[] positions)
+		{
+			if (positions != null && positions.Length != colors.Length)
+			{
+				throw new ArgumentException("PopUpViewAnimatedColors and locations should contain the same number of items.");
+			}
+
+			popUpViewAnimatedColors = colors;
+			keyTimes = KeyTimesFromSliderPositions(positions);
+
+			if (colors.Length >= 2)
+			{
+				PopUpView.SetAnimatedColors(colors, keyTimes);
+			}
+			else
+			{
+				PopUpViewColor = colors.Last() ?? popUpViewColor;
+			}
+		}
+
+
+		public nfloat PopUpViewCornerRadius
+		{
+			get { return PopUpView.CornerRadius; }
+			set
+			{
+				PopUpView.CornerRadius = value;
+			}
+		}
+
+
+		public nfloat PopUpViewArrowLength
+		{
+			get { return PopUpView.ArrowLength; }
+			set
+			{
+				PopUpView.ArrowLength = value;
+			}
+		}
+
+
+		public nfloat PopUpViewWidthPaddingFactor
+		{
+			get
+			{
+				return PopUpView.WidthPaddingFactor;
+			}
+			set
+			{
+				PopUpView.WidthPaddingFactor = value;
+			}
+		}
+
+
+		public nfloat PopUpViewHeightPaddingFactor
+		{
+			get { return PopUpView.HeightPaddingFactor; }
+			set
+			{
+				PopUpView.HeightPaddingFactor = value;
+			}
+		}
+
+
+		private void Setup()
+		{
+			autoAdjustTrackColor = true;
+			valueRange = MaxValue - MinValue;
+			PopUpViewAlwaysOn = false;
+
+			var formatter = new NSNumberFormatter();
+			formatter.NumberStyle = NSNumberFormatterStyle.Decimal;
+			formatter.RoundingMode = NSNumberFormatterRoundingMode.HalfUp;
+			formatter.MaximumFractionDigits = 2;
+			formatter.MinimumFractionDigits = 2;
+			numberFormatter = formatter;
+
+			PopUpView = new ValuePopUpView(CGRect.Empty);
+			popUpViewColor = UIColor.FromHSBA(0.6f, 0.6f, 0.5f, 0.8f);
+
+			PopUpView.Alpha = 0.0f;
+			PopUpView.Delegate = this;
+			this.AddSubview(PopUpView);
+
+			TextColor = UIColor.White;
+			Font = UIFont.BoldSystemFontOfSize(22);
+		}
+
+
+		public override float MaxValue
+		{
+			get
+			{
+				return base.MaxValue;
+			}
+			set
+			{
+				base.MaxValue = value;
+				valueRange = MaxValue - MinValue;
+			}
+		}
+
+		public override float MinValue
+		{
+			get
+			{
+				return base.MinValue;
+			}
+			set
+			{
+				base.MinValue = value;
+				valueRange = MaxValue - MinValue;
+			}
+		}
+
+		public void SetMaxFractionDigitsDisplayed(nint maxDigits)
+		{
+			numberFormatter.MaximumFractionDigits = maxDigits;
+			numberFormatter.MinimumFractionDigits = maxDigits;
+		}
+
+		private NSNumberFormatter numberFormatter;
+		public NSNumberFormatter NumberFormatter
+		{
+			get { return numberFormatter.Copy() as NSNumberFormatter; }
+			set
+			{
+				numberFormatter = value.Copy() as NSNumberFormatter;
+			}
+		}
+
+
+		// Present the popUpView manually, without touch event.
+		public void ShowPopUpView(bool animated)
+		{
+			PopUpViewAlwaysOn = true;
+			ShowPopUpViewAnimated(animated);
+		}
+
+		// The popUpView will not hide again until you call 'HidePopUpView'
+		public void HidePopUpView(bool animated)
+		{
+			PopUpViewAlwaysOn = false;
+			HidePopUpViewAnimated(animated);
+		}
+
+
+		/// IASValuePopUpViewDelegate
+
+		public void ColorDidUpdate(UIColor opaqueColor)
+		{
+			base.MinimumTrackTintColor = opaqueColor;
+		}
+
+		public nfloat CurrentValueOffset => (Value - MinValue) / valueRange;
+
+		public void UpdatePopUpView()
+		{
+			CGSize popUpViewSize;
+			string valueString = DataSource?.Slider(this, this.Value) ?? "";
+			if (!string.IsNullOrWhiteSpace(valueString))
+			{
+				popUpViewSize = PopUpView.PopUpSizeForString(valueString);
+			}
+			else
+			{
+				valueString = numberFormatter.StringFromNumber(this.Value);
+				popUpViewSize = CalculatePopUpViewSize();
+			}
+
+			// calculate the popUpView frame
+			CGRect thumbRect = ThumbRect();
+			nfloat thumbW = thumbRect.Size.Width;
+			nfloat thumbH = thumbRect.Size.Height;
+
+			CGRect popUpRect = thumbRect.Inset((thumbW - popUpViewSize.Width) / 2, (thumbH - popUpViewSize.Height) / 2);
+			popUpRect.Y = thumbRect.Y - popUpViewSize.Height;
+
+			// determine if popUpRect extends beyond the frame of the progress view
+			// if so adjust frame and set the center offset of the PopUpView's arrow
+			nfloat minOffsetX = popUpRect.GetMinX();
+			nfloat maxOffsetX = popUpRect.GetMaxX() - this.Bounds.Width;
+
+			nfloat offset = minOffsetX < 0.0f ? minOffsetX : (maxOffsetX > 0.0f ? maxOffsetX : 0.0f);
+			popUpRect.X -= offset;
+
+			PopUpView.SetFrame(popUpRect, offset, valueString);
+		}
+
+		public CGSize CalculatePopUpViewSize()
+		{
+			// negative values need more width than positive values
+			var minStr = new NSString(NumberFormatter.StringFromNumber(NSNumber.FromFloat(MinValue)));
+			CGSize minValSize = PopUpView.PopUpSizeForString(minStr);
+			var maxStr = new NSString(NumberFormatter.StringFromNumber(NSNumber.FromFloat(MaxValue)));
+			CGSize maxValSize = PopUpView.PopUpSizeForString(maxStr);
+
+			return minValSize.Width >= maxValSize.Width ? minValSize : maxValSize;
+		}
+
+		// takes an array of NSNumbers in the range self.minimumValue - self.maximumValue
+		// returns an array of NSNumbers in the range 0.0 - 1.0
+		public NSNumber[] KeyTimesFromSliderPositions(NSNumber[] positions)
+		{
+			if (positions == null) return null;
+
+			Array.Sort(positions);
+
+			var temp = new List<NSNumber>();
+			foreach (var num in positions)
+			{
+				temp.Add(NSNumber.FromNFloat((num.FloatValue - MinValue) / valueRange));
+			}
+			return temp.ToArray();
+		}
+
+		public CGRect ThumbRect()
+		{
+			return ThumbRectForBounds(this.Bounds, this.TrackRectForBounds(this.Bounds), this.Value);
+		}
+
+		public void ShowPopUpViewAnimated(bool animated)
+		{
+			Delegate?.SliderWillDisplayPopUpView(this);
+			PopUpView.ShowAnimated(animated);
+		}
+
+		public void HidePopUpViewAnimated(bool animated)
+		{
+			Delegate?.SliderWillHidePopUpView(this);
+			PopUpView.HideAnimated(animated, () =>
+			{
+				Delegate?.SliderDidHidePopUpView(this);
+			});
+		}
+
+
+		/// override
+
+		public override void LayoutSubviews()
+		{
+			base.LayoutSubviews();
+
+			UpdatePopUpView();
+		}
+
+		public override void MovedToWindow()
+		{
+			base.MovedToWindow();
+
+			if (this.Window == null)  // removed from window - cancel notifications
+			{
+				NSNotificationCenter.DefaultCenter.RemoveObserver(this);
+			}
+			else  // added to window - register notifications
+			{
+				if (PopUpViewAnimatedColors != null)  // restart color animation if needed
+				{
+					PopUpView.SetAnimatedColors(popUpViewAnimatedColors, keyTimes);
+				}
+
+				NSNotificationCenter.DefaultCenter.AddObserver(UIApplication.DidBecomeActiveNotification, (obj) =>
+				{
+					// ensure animation restarts if app is closed then becomes active again
+					if (popUpViewAnimatedColors != null)
+					{
+						PopUpView.SetAnimatedColors(popUpViewAnimatedColors, keyTimes);
+					}
+				});
+			}
+
+		}
+
+		public override float Value
+		{
+			get
+			{
+				return base.Value;
+			}
+			set
+			{
+				base.Value = value;
+
+				PopUpView.SetAnimationOffset(CurrentValueOffset, (opaqueReturnColor) =>
+				{
+					base.MinimumTrackTintColor = opaqueReturnColor;
+				});
+			}
+		}
+
+		public override void SetValue(float value, bool animated)
+		{
+			if (animated)
+			{
+				PopUpView.AnimateBlock((duration) =>
+				{
+					UIView.Animate(duration, () =>
+					{
+						base.SetValue(value, animated);
+						PopUpView.SetAnimationOffset(CurrentValueOffset, (opaqueReturnColor) =>
+						{
+							base.MinimumTrackTintColor = opaqueReturnColor;
+						});
+						LayoutIfNeeded();
+					});
+				});
+			}
+			else
+			{
+				base.SetValue(value, animated);
+			}
+		}
+
+		public override UIColor MinimumTrackTintColor
+		{
+			get
+			{
+				return base.MinimumTrackTintColor;
+			}
+			set
+			{
+				AutoAdjustTrackColor = false;  // if a custom value is set then prevent auto coloring
+				base.MinimumTrackTintColor = value;
+			}
+		}
+
+
+		public override bool BeginTracking(UITouch uitouch, UIEvent uievent)
+		{
+			bool begin = base.BeginTracking(uitouch, uievent);
+			if (begin && !PopUpViewAlwaysOn)
+			{
+				ShowPopUpViewAnimated(true);
+			}
+			return begin;
+		}
+
+		public override bool ContinueTracking(UITouch uitouch, UIEvent uievent)
+		{
+			bool continueTrack = base.ContinueTracking(uitouch, uievent);
+			if (continueTrack)
+			{
+				PopUpView.SetAnimationOffset(CurrentValueOffset, (opaqueReturnColor) =>
+				{
+					base.MinimumTrackTintColor = opaqueReturnColor;
+				});
+			}
+			return continueTrack;
+		}
+
+		public override void CancelTracking(UIEvent uievent)
+		{
+			base.CancelTracking(uievent);
+			if (!PopUpViewAlwaysOn)
+			{
+				HidePopUpViewAnimated(true);
+			}
+		}
+
+		public override void EndTracking(UITouch uitouch, UIEvent uievent)
+		{
+			base.EndTracking(uitouch, uievent);
+			if (!PopUpViewAlwaysOn)
+			{
+				HidePopUpViewAnimated(true);
+			}
+		}
+	}
+}
+
